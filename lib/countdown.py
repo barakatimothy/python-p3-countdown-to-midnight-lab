00ad45d8ff4a1eb068a77a4c621d60af0940dea4
barakatimothy <timothybaraka39@gmail.com>
2023-08-22 01:47:59 -0400

    complete!

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,13 +1,20 @@
 import sys
 import io
-def countdown(starting_number):
-    count = starting_number
-    while count < 1:
-        print(f'{count} SECOND(S)!')
-        count = count - 1 
-    return "HAPPY NEW YEAR!"
+import time
+def countdown(number):
+    for i in range(number, 0, -1):
+        print(f"{i} SECOND(S)!")
+    print("HAPPY NEW YEAR!")
 
 countdown(10)
 
-def countdown_with_sleep(seconds):
-    pass
+
+def countdown_with_sleep(number):
+    for i in range(number, 0, -1):
+        print(f"{i} SECOND(S)!")
+        time.sleep(1)  # Sleep for 1 second
+    print("HAPPY NEW YEAR!")
+
+
+countdown_with_sleep(10)
+    
